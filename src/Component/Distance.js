ab15affcb8bc31f456fc947288d25a593982d407
developereswar <developereswar94@gmail.com>
2020-03-13 09:41:32 +0530

    distance_chnages

--- src/Component/Distance.js
+++ src/Component/Distance.js
@@ -3,10 +3,7 @@ import React from "react";
 const Distance = (props) =>{
     let distance = null||props.PlanetDist;
     return(
-            <h1>
-                <br/>
-           {distance || "0 Miles" }
-           </h1>
+           distance || "0 Miles" 
     )
 }
 
